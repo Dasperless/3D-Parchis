c89a19dc27d753c69bc8025f5d8959b662c50f37
Darío Vargas <dariovm13@gmail.com>
2021-11-26 23:53:46 -0600

    [CHG] Traducción de python a js

---
+++ Programa/logica/Color.js
@@ -0,0 +1,6 @@
+module.exports = {
+    AMARILLO :1,
+    AZUL : 2,
+    ROJO : 3,
+    VERDE : 4
+}
--- Programa/logica/test.py
+++
@@ -1,9 +0,0 @@
-from Parchis import Parchis
-from Jugador import Color
-
-p = Parchis()
-p.agregarJugador("Juan")
-p.agregarJugador("Darío")
-
-p.ordenarJugadores()
-print(p.iniciarPartida())
